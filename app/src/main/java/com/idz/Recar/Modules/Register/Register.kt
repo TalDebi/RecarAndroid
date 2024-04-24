aab062a6664331d0a353d32eab9cdb3a108eb25c
unknown <taldebi2@gmail.com>
2024-04-24 21:02:29 +0300

    trying to show user data

--- app/src/main/java/com/idz/Recar/Modules/Register/Register.kt
+++ app/src/main/java/com/idz/Recar/Modules/Register/Register.kt
@@ -1,4 +1,5 @@
 package com.idz.Recar.Modules.Register
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,9 +14,11 @@ import android.widget.TextView
 import androidx.navigation.Navigation
 import com.idz.Recar.Model.Model
 import com.idz.Recar.Model.Student
+import com.idz.Recar.Model.User
 import com.idz.Recar.Modules.Login.LoginDirections
 import com.idz.Recar.Modules.Students.StudentsFragmentDirections
 import com.idz.Recar.R
+import com.idz.Recar.Utils.SharedPreferencesHelper
 
 class Register : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +42,28 @@ class Register : Fragment() {
     }
 
     private fun setupUI(view: View) {
+        val nameEditText: EditText = view.findViewById(R.id.nameEditText)
+        val emailEditText: EditText = view.findViewById(R.id.emailEditText)
+        val phoneNumberEditText: EditText = view.findViewById(R.id.phoneNumberEditText)
+        val passwordEditText: EditText = view.findViewById(R.id.passwordEditText)
+        val confirmPasswordEditText: EditText = view.findViewById(R.id.confirmPasswordEditText)
+        val registerButton: Button = view.findViewById(R.id.registerButton)
+
+        registerButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val phoneNumber = phoneNumberEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            val confirmPassword = confirmPasswordEditText.text.toString()
+
+            // Validate inputs here
+
+            val user = User(name, email, phoneNumber, password)
+            Model.instance.addUser(user) {
+                // Handle registration completion
+                SharedPreferencesHelper.saveUserId(requireContext(), user.id)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
